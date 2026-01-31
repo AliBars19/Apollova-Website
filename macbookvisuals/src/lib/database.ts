8864922734b03ff1e9e3d7f4369ad93ab71651f9
AliBars19 <Ali.Bars@city.ac.uk>
2026-01-31 09:27:51 +0000

    dafdfs

--- macbookvisuals/src/lib/database.ts
+++ macbookvisuals/src/lib/database.ts
@@ -108,3 +108,7 @@ export function generateLicenseKey(): string {
   
   return segments.join('-');
 }
+
+export function generateId(): string {
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+}
